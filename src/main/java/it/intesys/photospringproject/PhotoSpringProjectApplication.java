4ea692f45c9d1c70d5758776af2d51cbea766a01
Jacopo-Sassi <sassijacopp@gmail.com>
2025-06-10 09:29:18 +0200

    Creato Progetto PhotoSpringBoot (SpringBoot + Maven)

---
+++ src/main/java/it/intesys/photospringproject/PhotoSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package it.intesys.photospringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/intesys/photospringproject/PhotoSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.intesys.photospringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
