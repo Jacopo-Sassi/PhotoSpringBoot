66fdea5966b1ef984e0d4ae1e4da1ba94998425a
Jacopo-Sassi <sassijacopp@gmail.com>
2025-06-16 11:48:56 +0200

    creato il costruttore della lista photo in PhotoRestController per usare un unica lista

--- src/main/java/it/intesys/photospringproject/controller/PhotoRestController.java
+++ src/main/java/it/intesys/photospringproject/controller/PhotoRestController.java
@@ -2,20 +2,33 @@ package it.intesys.photospringproject.controller;
 
 import it.intesys.photospringproject.model.Photo;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PhotoRestController {
-    @RequestMapping("api/photos")
-    public Iterable<Photo> getAll() {
-        List <Photo> list = new ArrayList<Photo>();
+    private List<Photo> list;
+
+    public PhotoRestController() {
+        list = new ArrayList<Photo>();
         list.add(new Photo(1,"./img/01.png"));
         list.add(new Photo(2,"./img/02.png"));
         list.add(new Photo(3,"./img/03.png"));
+    }
+    @RequestMapping("api/photos")
+    public Iterable<Photo> getAll() {
+
         return list;
+    }
+    @RequestMapping("api/photos/{id}")
+    public Photo getById(@PathVariable int id) {
+
+        Optional<Photo> photo = list.stream().filter(p -> p.getId() == id).findFirst();
+        return photo.orElseThrow(() -> new RuntimeException("Photo not found with id: " + id));
     }
 }
