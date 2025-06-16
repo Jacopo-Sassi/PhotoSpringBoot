bdcdbca2bcb8bb1dd3bedd4406feecb354868d52
Jacopo-Sassi <sassijacopp@gmail.com>
2025-06-16 10:34:56 +0200

    Aggiunti log su application.properties e creato il controller HomeController

---
+++ src/main/java/it/intesys/photospringproject/controller/HomeController.java
@@ -0,0 +1,21 @@
+package it.intesys.photospringproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Welcome to the Photo Spring Project!";
+    }
+
+    @RequestMapping("/demo")
+    @ResponseBody
+    public String demo() {
+        return "This is a demo endpoint!";
+    }
+
+}
