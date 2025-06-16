29373b4e5b4362f6c5efbe3a2bb56b8a888ed05d
Jacopo-Sassi <sassijacopp@gmail.com>
2025-06-16 11:37:00 +0200

    aggiunte foto di esempio e aggiunta funzionalità di poter vedere solo le foto selezionate su index.html

--- src/main/java/it/intesys/photospringproject/controller/PhotoRestController.java
+++ src/main/java/it/intesys/photospringproject/controller/PhotoRestController.java
@@ -1,6 +1,7 @@
 package it.intesys.photospringproject.controller;
 
 import it.intesys.photospringproject.model.Photo;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,12 @@ import java.util.List;
 
 @RestController
 public class PhotoRestController {
-    @RequestMapping("/photos")
+    @RequestMapping("api/photos")
     public Iterable<Photo> getAll() {
         List <Photo> list = new ArrayList<Photo>();
-        list.add(new Photo(1,"./img/27.png"));
-        list.add(new Photo(2,"./img/26.png"));
-        list.add(new Photo(3,"./img/25.png"));
+        list.add(new Photo(1,"./img/01.png"));
+        list.add(new Photo(2,"./img/02.png"));
+        list.add(new Photo(3,"./img/03.png"));
         return list;
     }
 }
